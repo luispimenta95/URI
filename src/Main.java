aa7d701b4069b8a66791f7c2756ba47ae7335785
luispimenta95 <luisfelipearaujopimenta@gmail.com>
2020-03-27 16:09:12 -0300

    Subindo Arquivos

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			Scanner leia = new Scanner(System.in);
+			int a,b,c;
+			a = leia.nextInt();
+			b = leia.nextInt();
+			c = a+b;
+			System.out.println(c);
+	
+	}
+
+}
